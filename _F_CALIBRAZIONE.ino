70671d1e6020c6743812a5c70bb4a2ec8c1d24ad
UmbyWanKenobi <umbywankenobi@hotmail.com>
2016-03-01 17:56:59 +0100

    Added files via upload

--- _F_CALIBRAZIONE.ino
+++ _F_CALIBRAZIONE.ino
@@ -2,7 +2,9 @@
 void _F_CALIBRAZIONE() {
   boolean calibrazione = true;
   unsigned int _deg, _D, _min, _M;
+  lcd.clear();
 
+  
   while (calibrazione) {
     sensors_event_t event;
     compass.getEvent(&event);
@@ -13,18 +15,11 @@ void _F_CALIBRAZIONE() {
 
     float ago = atan2(Asse_y, Asse_x) + DECL_MAGN;   // Valore espresso in radianti
 
-
     // Corregge un valore negativo
     if (ago < 0)   ago += 2 * pi;
     // Controlla la scala.
     if (ago > 2 * pi)ago -= 2 * pi;
-    // Convert radians to degrees for readability.
-    //float gradi_ago = GRADI_DA_RADIANTI(ago);
-    //Convert float to int
-    // int _D=(int)gradi_ago;
-    lcd.clear();
-    lcd.setCursor(0, 0);
-
+    clear_row(0);
     lcd.print("X");
     lcd.print(Asse_x);
     lcd.print(" Y ");
@@ -34,43 +29,60 @@ void _F_CALIBRAZIONE() {
 
 
     lcd.setCursor(0, 1);
-    lcd.print("ANGOLO: ");
+
     Split_into_dms(ago, &_deg, &_min);
     _D = _deg;
     _M = _min;
+    sprintf(_buffer, "ANGOLO: %3d\337%2d'", _D, _M);
 
-    sprintf(_buffer, "%3d\337%2d'", _D, _M);
-
-lcd.setCursor(0, 2);
+    clear_row(1);
+    lcd.print( _buffer );
+    clear_row(2);
     lcd.print("Radianti: ");
     lcd.print(ago);
-    lcd.setCursor(5, 3);
-    if (_D >= 0 && _D <= 45) {   // 0-45 degrees
-      lcd.print("NORD");
+
+    if (( _D >= 338 && _D <= 360 ) or (_D >= 0 && _D <=  22 )) {      // NORD
+      clear_row(3);
+      lcd.setCursor((20 - strlen(MENU[3][0])) / 2, 3);
+      lcd.print( MENU[3][0] );
     }
-    else if (_D > 45 && _D <= 90) { // 46-90 degrees
-      lcd.print("NORD-OVEST");
+    else if ( _D >=  23 &&  _D <=  67 ) {                       // NORD-OVEST
+      clear_row(3);
+      lcd.setCursor((20 - strlen(MENU[3][1])) / 2, 3);
+      lcd.print( MENU[3][1] );
     }
-    else if (_D > 90 && _D <= 135) { // 91-135 degrees
-      lcd.print("OVEST");
+    else if ( _D >=  68 && _D <=  112 ) {                            // OVEST
+      clear_row(3);
+      lcd.setCursor((20 - strlen(MENU[3][2])) / 2, 3);
+      lcd.print( MENU[3][2] );
     }
-    else if (_D > 135 && _D <= 180) { // 136-180 degrees
-      lcd.print("SUD-OVEST");
+    else if ( _D >=  113 && _D <=  157 ) {                       // SUD-OVEST
+      clear_row(3);
+      lcd.setCursor((20 - strlen(MENU[3][3])) / 2, 3);
+      lcd.print( MENU[3][3] );
     }
-    else if (_D > 180 && _D <= 225) { // 181-225 degrees
-      lcd.print("SUD");
+    else if ( _D >=  158 && _D <=  202 ) {                            // SUD
+      clear_row(3);
+      lcd.setCursor((20 - strlen(MENU[3][4])) / 2, 3);
+      lcd.print( MENU[3][4] );
     }
-    else if (_D > 225 && _D <= 270) { // 226-270 degrees
-      lcd.print("SUD-EST");
+    else if ( _D >=  203 && _D <=  247 ) {                         // SUD-EST
+      clear_row(3);
+      lcd.setCursor((20 - strlen(MENU[3][5])) / 2, 3);
+      lcd.print( MENU[3][5] );
     }
-    else if (_D > 270 && _D <= 315) { // 271-315 degrees
-      lcd.print("EST");
+    else if ( _D >=  248 && _D <=  292 ) {                             // EST
+      clear_row(3);
+      lcd.setCursor((20 - strlen(MENU[3][6])) / 2, 3);
+      lcd.print( MENU[3][6] );
     }
-    else if (_D > 315 && _D <= 360) { // 316-360 degrees
-      lcd.print("NORD-EST");
+    else if ( _D >=  293 && _D <=  337 ) {                        // NORD-EST
+      clear_row(3);
+      lcd.setCursor((20 - strlen(MENU[3][7])) / 2, 3);
+      lcd.print( MENU[3][7] );
     }
-    delay(500);
+    delay(1000);
   }
-  //////////////////////////////////////////////////////////////////////////
+
 }
 
